8c1223a40fff703eb48758a59cf331aff8167610
vanik <vanik_888@mail.ru>
2015-10-11 19:00:33 +0300

    added player and left-right movement

--- game.py
+++ game.py
@@ -4,11 +4,13 @@
 import pygame
 from pygame import *
 from blocks import *
+from player import *
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 640
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
 BACKGROUND_COLOR = "#004000"
+
 
 def main():
     pygame.init() # Инициация PyGame, обязательная строчка
@@ -17,6 +19,11 @@ def main():
     bg = Surface((WIN_WIDTH, WIN_HEIGHT)) # Создание видимой поверхности
                                           # будем использовать как фон
     bg.fill(Color(BACKGROUND_COLOR))      # Заливаем поверхность сплошным цветом
+
+    timer = pygame.time.Clock()
+    hero = Player(55, 55)
+    left = False
+    right = False
 
 
     level = [
@@ -44,9 +51,20 @@ def main():
 
 
     while 1: # Основной цикл программы
+        timer.tick(60)
         for e in pygame.event.get(): # Обрабатываем события
             if e.type == QUIT:
                 raise SystemExit, "QUIT"
+            if e.type == KEYDOWN and e.key == K_LEFT:
+               left = True
+            if e.type == KEYDOWN and e.key == K_RIGHT:
+               right = True
+
+            if e.type == KEYUP and e.key == K_RIGHT:
+               right = False
+            if e.type == KEYUP and e.key == K_LEFT:
+                left = False
+
         screen.blit(bg, (0, 0))      # Каждую итерацию необходимо всё перерисовывать
         x = 0
         y = 0
@@ -60,6 +78,8 @@ def main():
             y += PLATFORM_HEIGHT
             x = 0
 
+        hero.update(left, right) # передвижение
+        hero.draw(screen) # отображение
 
         pygame.display.update()     # обновление и вывод всех изменений на экран
 
